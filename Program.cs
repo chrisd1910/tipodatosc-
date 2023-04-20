9adf11a316fa5afcb3e3d4df201fa21ee6d6246e
chrisd1910 <christian.santos.davila@gmail.com>
2023-04-20 11:40:34 -0500

    tipos de datos

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿/*Console.WriteLine("------------------------------------Ejercicio 1 ------------------------------------------");
+Console.WriteLine("ingrese su nombre");
+string nombre= Console.ReadLine();
+Console.WriteLine("Ingrese apellido");
+string apellido= Console.ReadLine();
+Console.WriteLine("Ingrese su edad");
+int edad = Convert.ToInt32(Console.ReadLine());
+string comentario = "Sabe programar";
+Console.WriteLine("Nombre: "+nombre+", Apellido: "+apellido +", Edad: "+edad+ ", Comentario: "+ comentario);
+
+
+Console.WriteLine("------------------------------------Ejercicio 2 ------------------------------------------");
+//COCHE
+int puerta = 4;
+int ruedas = 4;
+string marca = "Toyota";
+bool Itv = true;
+Console.WriteLine("Puerta: " + puerta + ", Ruedas: "+ruedas+", Marca: "+marca+", Itv: "+Itv);
+//MESA
+
+float peso = 15.4f;
+float largo = 2.8f;
+string material = "Madera";
+string color = "Rojo";
+
+Console.WriteLine("Peso: " + peso + ", Largo: " + largo + ", Material: " + material + ", Color: " + color);
+*/
+Console.WriteLine("------------------------------------Ejercicio 3 ------------------------------------------");
+
+//numero mayor o igual que 18
+
+int num = 20;
+int num2 = 10;
+char c= 'a';
+Console.WriteLine("Mayor o igual que 18:  "+(num>=18));
+Console.WriteLine("Char es igual 'a':  " + (c=='a'));
+Console.WriteLine("true - true:  " + (num2 > 8 && num > 10));
+Console.WriteLine("true- false: " + (num2 > 8 && num > 30));
+
+
+/*Console.WriteLine("Ingresa tu nombre");
+string nombre = Console.ReadLine();
+
+//edad
+Console.WriteLine("Ingresa tu edad");
+int edad = Convert.ToInt32 (Console.ReadLine());
+
+//ciudad
+Console.WriteLine("introduce tu ciudad");
+string ciudad= Console.ReadLine();
+
+//presentar mensaje
+
+Console.Write ("Nombre: "+ nombre);
+Console.Write(", Edad: "+ edad);
+Console.Write(", Ciudad: " + ciudad);
+
+
+int? i = null;
+
+Console.WriteLine(i.HasValue);
+
+
+int i = 0;
+long l = 1;
+float f = 1.55555f;
+double d = 2.666666d;
+char c = 'd';
+string str = "prueba de cadena";
+
+Console.Write("Entero: " + i);
+Console.Write("Long: " + l);
+Console.Write("Float: " + f);
+Console.Write("Double: " + d);
+Console.Write("Char: " + c);
+Console.Write("String: " + str);
+
+Console.WriteLine("Entero: "+i);
+Console .WriteLine("Long: " +l);
+Console.WriteLine("Float: " + f);
+Console.WriteLine("Double: "+ d);
+Console.WriteLine("Char: "+ c);
+Console.WriteLine("String: " + str);
+*/
